1fff115507eaadf5b90c877153ad4e527ff078b8
Liao Zhu <liao.zhu.cs@gmail.com>
2023-01-06 19:24:14 -0600

    clean up

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::fs::create_dir_all;
 use std::env;
 use std::fs::write;
-use std::fs::read_to_string;
 use std::collections::HashMap; // [Semester: [courses]]
 
 fn main() {
@@ -13,18 +12,19 @@ fn main() {
 
     let mut map: HashMap<String, Vec<&str>> = HashMap::new();
     map.insert(String::from("Spring 2023"), vec!["Algo", "Analysis", "Gen Ed"]);
-    let mut v: Vec<i32> = Vec::new();
-    let mut test_vector: Vec<&str> = vec!["Hello", "This", "Is", "a", "test"];
-    for course in  test_vector{
-        let file_name = format!("./Courses/{}.md", course);
-        let mut file = File::create(&file_name)
-            .expect("Error creating file...");   
-        // file.write_all(b"# Test")
-        //     .expect("Error writing to file..."); 
-        write(format!("./Courses/{}.md", course), format!("# {}", course));
+    if let Some(courses) = map.get("Spring 2023"){
+        for course in courses {
+            // Create md files of each course
+            let file_name = format!("./Courses/{}.md", course);
+            File::create(&file_name).expect("Error creating file...");   
+
+            // Write title on each md file
+            write(format!("./Courses/{}.md", course), format!("# {}", course));
+        }
     }
-    // for (key, value) in map {
-    //     println!("Semester: {}", key);
-    //     println!("Courses: {:?}", value);
+    // for (key) in map.get("Spring 2023") {
+    //     for name in key {
+    //         println!("{}", name);
+    //     }
     // }
 }
