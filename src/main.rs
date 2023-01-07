0861007426269feb96db9bb0a5e1853ec71ab80d
Liao Zhu <liao.zhu.cs@gmail.com>
2023-01-06 19:38:42 -0600

    static input handled

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,36 @@ use std::fs::write;
 use std::collections::HashMap; // [Semester: [courses]]
 
 fn main() {
-    // Create directory "Courses" in external directory
     let mut current_dir = env::current_dir().unwrap();
-    current_dir.push("Courses");
-    create_dir_all(&current_dir).unwrap();
 
-    let mut map: HashMap<String, Vec<&str>> = HashMap::new();
-    map.insert(String::from("Spring 2023"), vec!["Algo", "Analysis", "Gen Ed"]);
-    if let Some(courses) = map.get("Spring 2023"){
-        for course in courses {
-            // Create md files of each course
-            let file_name = format!("./Courses/{}.md", course);
+
+    let mut schedule: HashMap<String, Vec<&str>> = HashMap::new();
+    schedule.insert(String::from("Spring 2023"), vec!["Algo", "Analysis", "Gen Ed"]);
+
+    // Create semester dir inside Courses
+    for (semester, _classes) in &schedule {
+        current_dir.push(format!("./{}", semester));
+        create_dir_all(&current_dir).unwrap();
+    }
+
+    for (semester, classes) in &schedule {
+        for class in classes {
+            let file_name = format!("./{}/{}.md", semester, class);
             File::create(&file_name).expect("Error creating file...");   
 
             // Write title on each md file
-            write(format!("./Courses/{}.md", course), format!("# {}", course));
+            write(format!("./{}/{}.md", semester, class), format!("# {}", class));
         }
     }
-    // for (key) in map.get("Spring 2023") {
-    //     for name in key {
-    //         println!("{}", name);
+
+    // if let Some(courses) = schedule.get("Spring 2023"){
+    //     for course in courses {
+    //         // Create md files of each course
+    //         let file_name = format!("./Courses/{}.md", course);
+    //         File::create(&file_name).expect("Error creating file...");   
+
+    //         // Write title on each md file
+    //         write(format!("./Courses/{}.md", course), format!("# {}", course));
     //     }
     // }
 }
