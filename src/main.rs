f4fd7f4bfcd224f46f184427c6c3cd0c5045d777
Liao Zhu <liao.zhu.cs@gmail.com>
2023-01-06 23:56:51 -0600

    Final

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,47 @@ use std::fs::create_dir_all;
 use std::env;
 use std::fs::write;
 use std::collections::HashMap; // [Semester: [courses]]
+use std::io;
 
 fn main() {
-    let mut current_dir = env::current_dir().unwrap();
 
+    // let mut input_map = HashMap::new();
+    println!("Enter 'Semester: Course1, Course2, Course3'");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+
+    // Split input into vector key-value pairs
+    let pairs: Vec<_> = input
+        .trim()
+        .split(": ")
+        .map(|s| s.split(", ").collect::<Vec<_>>())
+        .collect();
+
+    let mut classes = Vec::new();
+    for class in &pairs[1] {
+        classes.push(*class);
+    }
 
     let mut schedule: HashMap<String, Vec<&str>> = HashMap::new();
-    schedule.insert(String::from("Spring 2023"), vec!["Algo", "Analysis", "Gen Ed"]);
+    schedule.insert(format!("{}", pairs[0][0]), classes);
 
     // Create semester dir inside Courses
     for (semester, _classes) in &schedule {
-        current_dir.push(format!("./{}", semester));
+        let mut current_dir = env::current_dir().unwrap();
+        current_dir.push(format!("../{}", semester));
         create_dir_all(&current_dir).unwrap();
     }
 
+    // Add course md files to semester dir
     for (semester, classes) in &schedule {
         for class in classes {
-            let file_name = format!("./{}/{}.md", semester, class);
+            println!("{}", class);
+            let file_name = format!("../{}/{}.md", semester, class);
             File::create(&file_name).expect("Error creating file...");   
 
             // Write title on each md file
             write(format!("./{}/{}.md", semester, class), format!("# {}", class));
         }
     }
-
-    // if let Some(courses) = schedule.get("Spring 2023"){
-    //     for course in courses {
-    //         // Create md files of each course
-    //         let file_name = format!("./Courses/{}.md", course);
-    //         File::create(&file_name).expect("Error creating file...");   
-
-    //         // Write title on each md file
-    //         write(format!("./Courses/{}.md", course), format!("# {}", course));
-    //     }
-    // }
 }
