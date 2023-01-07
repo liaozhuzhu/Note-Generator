f2bb5dda7e30ee4c0d3d443d04afc97752b04baa
Liao Zhu <liao.zhu.cs@gmail.com>
2023-01-06 18:58:17 -0600

    Display md title

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 use std::fs::create_dir_all;
 use std::env;
-use std::io::Write;
+use std::fs::write;
+use std::fs::read_to_string;
 use std::collections::HashMap; // [Semester: [courses]]
 
 fn main() {
@@ -15,10 +16,12 @@ fn main() {
     let mut v: Vec<i32> = Vec::new();
     let mut test_vector: Vec<&str> = vec!["Hello", "This", "Is", "a", "test"];
     for course in  test_vector{
-        let mut file = File::create(format!("./Courses/{}.md", course))
+        let file_name = format!("./Courses/{}.md", course);
+        let mut file = File::create(&file_name)
             .expect("Error creating file...");   
-        file.write_all(b"# Test")
-            .expect("Error writing to file..."); 
+        // file.write_all(b"# Test")
+        //     .expect("Error writing to file..."); 
+        write(format!("./Courses/{}.md", course), format!("# {}", course));
     }
     // for (key, value) in map {
     //     println!("Semester: {}", key);
